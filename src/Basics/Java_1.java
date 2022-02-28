f1cd504c2d9777b50d9aca554d23cc69ea6566af
jalapathiqa <jalapathiqa@gmail.com>
2022-02-27 21:30:59 -0800

    java project first commit

---
+++ src/Basics/Java_1.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class Java_1 {
+	public static void main(String[] args) {
+		
+		for (int i = 0; i <10; i++) {
+			System.out.println(i);
+			
+		}
+		
+		System.out.println("***** ***** ************* *****");
+		
+		
+		for (int i = 100; i >=10; i--) {
+			System.out.println(i);
+			
+		}
+		
+	}
+
+}
